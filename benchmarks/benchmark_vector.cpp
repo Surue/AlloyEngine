19949e3e98df531dbb816479066797325131c672
Surue <nicolas06062000@hotmail.com>
2021-02-02 21:53:23 +0100

    Add googlebenchmark to project

---
+++ benchmarks/benchmark_vector.cpp
@@ -0,0 +1,69 @@
+#include <benchmark/benchmark.h>
+
+#include <vector>
+#include <random>
+#include <iostream>
+
+#include <vector.h>
+
+const long fromRange = 8;
+const long toRange = 1 << 15;
+const float maxNmb = 100.0f;
+
+using namespace alloy;
+
+void RandomFill(std::vector<math::fvec2>& numbers, float start = -maxNmb, float end = maxNmb)
+{
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::uniform_real_distribution<float> dist{start, end};
+    std::generate(numbers.begin(), numbers.end(), [&g, &dist]() { return math::fvec2(dist(g), dist(g)); });
+}
+
+void RandomFill(std::vector<math::fvec3>& numbers, float start = -maxNmb, float end = maxNmb)
+{
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::uniform_real_distribution<float> dist{ start, end };
+    std::generate(numbers.begin(), numbers.end(), [&g, &dist]() { return math::fvec3(dist(g), dist(g), dist(g)); });
+}
+
+static void BM_Vec2Magnitude(benchmark::State& state)
+{
+
+    std::vector<math::fvec2> local_numbers(state.range(0));
+    RandomFill(local_numbers, 0.0f, maxNmb);
+    for (auto _ : state)
+    {
+        for (auto v : local_numbers)
+        {
+            float f = v.Magnitude();
+            benchmark::DoNotOptimize(f);
+        }
+    }
+}
+
+BENCHMARK(BM_Vec2Magnitude)->Range(fromRange, toRange);
+
+static void BM_Vec3Magnitude(benchmark::State& state)
+{
+
+    std::vector<math::fvec3> local_numbers(state.range(0));
+    RandomFill(local_numbers, 0.0f, maxNmb);
+    for (auto _ : state)
+    {
+        for (auto v : local_numbers)
+        {
+            float f = v.Magnitude();
+            benchmark::DoNotOptimize(f);
+        }
+    }
+}
+
+BENCHMARK(BM_Vec3Magnitude)->Range(fromRange, toRange);
+
+int main(int argc, char** argv)
+{
+    ::benchmark::Initialize(&argc, argv);
+    ::benchmark::RunSpecifiedBenchmarks();
+}
