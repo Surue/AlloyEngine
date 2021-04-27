30323f79d4b00aaf23c0ed14825f6761ea90d87f
Surue <nicolas06062000@hotmail.com>
2021-04-27 22:05:07 +0200

    Add function to transform fvec2 to sf::Vector2

--- engine/modules/graphics/include/sfml_graphics_extension.h
+++ engine/modules/graphics/include/sfml_graphics_extension.h
@@ -11,4 +11,8 @@ inline sf::Color ColorToSfColor(const Color& color) {
 inline Color SfColorToColor(const sf::Color& color) {
 	return Color(color.r, color.g, color.b, color.a);
 }
+
+inline sf::Vector2f Fvec2ToSfVector2f(const math::fvec2 v) {
+	return sf::Vector2f(v.x, v.y);
+}
 }
